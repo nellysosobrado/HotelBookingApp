ce1decccd479259ef950dd40ca0bcea8e1279666
Nelly <Nelly.sosobrado@gmail.com>
2024-11-24 12:22:33 +0100

    Added error handling incase there's an error to connect to database

--- HotelBookingApp/Program.cs
+++ HotelBookingApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;//Makes it possible to use the databse that i am using from SSMS
+using Microsoft.Extensions.Configuration; //Makes it possible to load my configuration information that I have stored in my Json File
+using Microsoft.Extensions.DependencyInjection; // Makes it possible to use the build in dependency injection
+using System.Threading;
 using System;
 
 namespace HotelBookingApp
@@ -10,18 +11,53 @@ namespace HotelBookingApp
         private static ServiceProvider serviceProvider;
         static void Main(string[] args)
         {
-            //Lods the configuration from appsettings.json
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+            try 
+            {
+                Console.WriteLine("Loads the configuration from appsettings.json file...");
+                Thread.Sleep(5000);
 
-            //Creates service provider
-            serviceProvider = new ServiceCollection()
-                .AddDbContext<AppDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")))
-                .BuildServiceProvider();
+                //Loads, reads my appsettings.json file as a configuration. Sets up
+                var configuration = new ConfigurationBuilder()
+                    //Json file contains the connectionstring variable, where i stored the information the application needs to connect to the databvase
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();//calls build
+                Console.WriteLine("Configuration, has been completed. Trying to connect to the database...");
+                Thread.Sleep(5000);
 
-            RunMenu();
+
+                //Creates service provider
+                serviceProvider = new ServiceCollection()
+                    .AddDbContext<AppDbContext>(options =>
+                        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")))
+                    .BuildServiceProvider();
+
+                //test database connetion
+                using (var context = serviceProvider.GetService<AppDbContext>())
+                {
+                    if(context.Database.CanConnect())
+                    {
+                        Console.WriteLine("Connected to database succeded!");
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not connect to the database");
+                    }
+                }
+                RunMenu();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program is exiting.. press any key");
+                Console.ReadKey();
+            }
+           
+
+
         }
 
         static void RunMenu()
