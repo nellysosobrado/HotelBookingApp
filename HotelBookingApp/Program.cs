5501445a2b0da16e6b5745becfc9b4b06f4755ff
Nelly <Nelly.sosobrado@gmail.com>
2024-11-24 10:11:58 +0100

    Add project files.

---
+++ HotelBookingApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer;
+
+namespace HotelBookingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Database=HotelBookingDB;Trusted_Connection=True;TrustServerCertificate=true";
+
+            try
+            {
+                Console.WriteLine("Försöker ansluta till databasen...");
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Anslutning lyckades!");
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"SQL Error: {sqlEx.Message}");
+                Console.WriteLine($"Error Code: {sqlEx.Number}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Fel: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Programmet avslutas. Tryck på valfri tangent...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
