c4b827a6114aaa6139ad8df53de37d1a7c1cab27
Nelly <Nelly.sosobrado@gmail.com>
2024-12-17 07:11:09 +0100

    SQL queries, added queries where, sort and select

--- SQLQUERYHOTELBOOKINGDB.sql
+++ SQLQUERYHOTELBOOKINGDB.sql
@@ -47,6 +47,105 @@ UPDATE Rooms SET IsAvailable =1; -- All room available
 DELETE FROM Bookings; --Remove all current bookings
 DBCC CHECKIDENT ('Bookings', RESEED, 0); -- resetest the ID seeding 
 
+-- G
+
+-- SELECT
+SELECT 
+    B.BookingId, 
+    G.FirstName + ' ' + G.LastName AS GuestName, 
+    R.RoomId, 
+    R.Type AS RoomType, 
+    B.CheckInDate, 
+    B.CheckOutDate
+FROM 
+    Bookings AS B
+INNER JOIN 
+    Guests AS G ON B.GuestId = G.GuestId
+INNER JOIN 
+    Rooms AS R ON B.RoomId = R.RoomId
+WHERE 
+    G.LastName = 'Lastname2' 
+    AND B.IsCheckedOut = 0;
+
+-- SORT
+
+SELECT 
+    RoomId, 
+    Type AS RoomType, 
+    PricePerNight, 
+    SizeInSquareMeters, 
+    ExtraBeds
+FROM 
+    Rooms
+WHERE 
+    IsAvailable = 1
+ORDER BY 
+    PricePerNight ASC;
+
+--where
+SELECT 
+    B.BookingId, 
+    G.FirstName + ' ' + G.LastName AS GuestName, 
+    R.RoomId, 
+    B.CheckInDate, 
+    B.CheckOutDate
+FROM 
+    Bookings AS B
+INNER JOIN 
+    Guests AS G ON B.GuestId = G.GuestId
+INNER JOIN 
+    Rooms AS R ON B.RoomId = R.RoomId
+WHERE 
+    B.CheckInDate BETWEEN '2024-01-01' AND '2024-01-31'
+ORDER BY 
+    B.CheckInDate ASC;
+
+
+--WHERE
+SELECT 
+    GuestId, 
+    FirstName, 
+    LastName, 
+    Email, 
+    PhoneNumber
+FROM 
+    Guests
+WHERE 
+    LastName LIKE 'J%'
+ORDER BY 
+    LastName ASC;
+
+
+-- SELECT & WHERE
+SELECT
+    RoomId, 
+    Type AS RoomType, 
+    PricePerNight
+FROM 
+    Rooms
+WHERE 
+    Type IN ('Single', 'Double')
+ORDER BY 
+    PricePerNight DESC;
+
+
+-- SELECT & WHERE
+
+SELECT 
+    BookingId, 
+    RoomId, 
+    GuestId, 
+    CheckInDate, 
+    CheckOutDate
+FROM 
+    Bookings
+WHERE 
+    CheckOutDate IS NULL
+ORDER BY 
+    CheckInDate DESC;
+
+
+
 
 -- JOINS
 -- information om gäster, deras bokningar och vilka rum de har bokat.
