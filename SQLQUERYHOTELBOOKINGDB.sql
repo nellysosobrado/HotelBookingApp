d9432a38a1df4ddb4a8d2a6eb7f458e3c668ab25
Nelly <Nelly.sosobrado@gmail.com>
2024-12-17 07:06:39 +0100

    SQL file, has beeen updated. There's now sql queries with group, joins and subquery

--- SQLQUERYHOTELBOOKINGDB.sql
+++ SQLQUERYHOTELBOOKINGDB.sql
@@ -46,3 +46,71 @@ SELECT * FROM Bookings
 UPDATE Rooms SET IsAvailable =1; -- All room available
 DELETE FROM Bookings; --Remove all current bookings
 DBCC CHECKIDENT ('Bookings', RESEED, 0); -- resetest the ID seeding 
+
+
+-- JOINS
+-- information om gäster, deras bokningar och vilka rum de har bokat.
+
+SELECT 
+    G.GuestId, 
+    G.FirstName + ' ' + G.LastName AS GuestName,
+    B.BookingId,
+    R.RoomId,
+    R.Type AS RoomType,
+    R.PricePerNight,
+    B.CheckInDate,
+    B.CheckOutDate
+FROM 
+    Guests AS G
+INNER JOIN 
+    Bookings AS B ON G.GuestId = B.GuestId
+INNER JOIN 
+    Rooms AS R ON B.RoomId = R.RoomId
+WHERE 
+    B.IsCheckedOut = 0 -- Endast aktiva bokningar
+ORDER BY 
+    G.GuestId;
+
+
+-- GROUP BY 
+-- totala antalet bokningar och det totala intäktsvärdet per rumstyp.
+SELECT 
+    R.Type AS RoomType,
+    COUNT(B.BookingId) AS TotalBookings,
+    SUM(R.PricePerNight) AS TotalRevenue
+FROM 
+    Bookings AS B
+INNER JOIN 
+    Rooms AS R ON B.RoomId = R.RoomId
+GROUP BY 
+    R.Type
+ORDER BY 
+    TotalRevenue DESC;
+
+
+-- Unpaid bookings
+-- hämta alla obetalda bokningar med fakturor som är äldre än 10 dagar
+SELECT 
+    B.BookingId,
+    G.FirstName + ' ' + G.LastName AS GuestName,
+    R.RoomId,
+    I.TotalAmount,
+    I.PaymentDeadline
+FROM 
+    Bookings AS B
+INNER JOIN 
+    Guests AS G ON B.GuestId = G.GuestId
+INNER JOIN 
+    Rooms AS R ON B.RoomId = R.RoomId
+INNER JOIN 
+    Invoices AS I ON B.BookingId = I.BookingId
+WHERE 
+    I.IsPaid = 0 
+    AND I.PaymentDeadline < DATEADD(DAY, -10, GETDATE()) 
+    AND B.BookingId IN (
+        SELECT BookingId 
+        FROM Invoices 
+        WHERE IsPaid = 0
+    )
+ORDER BY 
+    I.PaymentDeadline ASC;
