a689b48e8598495bc0280dad34b3d5eefe84194f
Nelly <Nelly.sosobrado@gmail.com>
2024-12-31 14:52:06 +0100

    QUERIES

--- AssignmentFiles/SQL_QUERIES.sql
+++ AssignmentFiles/SQL_QUERIES.sql
@@ -1,38 +1,33 @@
 USE HotelBookingDB;
 GO
 
--- Invoices/Payments
+
+--2: (G) Skapa MS-SQL syntaxer som kan utföra ’Select’, ’Where’, ’Order By’ uppgifter mot din databas.
+
+
+-- Select
 SELECT * FROM Rooms
 SELECT * FROM Guests
 SELECT * FROm Bookings
 SELECT * FROM Invoices;
 SELECT * FROM Payments;
 SELECT * FROM CanceledBookingsHistory
-SELECT 
-    g.GuestId,
-    g.FirstName + ' ' + g.LastName AS GuestName,
-    b.BookingId,
-    r.RoomId,
-    r.Type AS RoomType,
-    i.TotalAmount,
-    CASE 
-        WHEN i.IsPaid = 1 THEN 'Paid'
-        ELSE 'Unpaid'
-    END AS PaymentStatus
-FROM Guests g
-JOIN Bookings b ON g.GuestId = b.GuestId
-JOIN Rooms r ON b.RoomId = r.RoomId
-LEFT JOIN Invoices i ON b.BookingId = i.BookingId
-ORDER BY g.GuestId;
+
+-- Where
+
+-- Visar alla betalda invoices
+SELECT * 
+FROM Invoices
+WHERE isPaid = 1
+
+-- -- Visar alla obetalda invoices
+SELECT * 
+FROM Invoices
+WHERE isPaid = 1
 
 
-
-SELECT i.InvoiceId
-FROM Invoices i
-LEFT JOIN Payments p ON i.InvoiceId = p.InvoiceId
-WHERE p.InvoiceId IS NULL;
-
-
+--Order By
+-- sorterar utifrån invoiceid's
 SELECT 
     i.InvoiceId,
     i.TotalAmount,
@@ -48,138 +43,11 @@ LEFT JOIN
 ORDER BY 
     i.InvoiceId;
 
--- ========================
--- 2. ÖVERSIKT AV GÄSTER, BOKNINGAR, BETALNINGSSTATUS
--- ========================
-SELECT 
-    g.GuestId,
-    g.FirstName,
-    g.LastName,
-    g.Email,
-    g.PhoneNumber,
-    b.BookingId,
-    b.RoomId,
-    b.CheckInDate,
-    b.CheckOutDate,
-    b.IsCheckedIn,
-    b.IsCheckedOut,
-    b.BookingStatus,
-    i.TotalAmount AS InvoiceAmount,
-    CASE 
-        WHEN i.IsPaid = 1 THEN 'Paid'
-        WHEN i.IsPaid = 0 THEN 'Unpaid'
-        ELSE 'No Invoice'
-    END AS PaymentStatus
-FROM 
-    Guests g
-LEFT JOIN 
-    Bookings b ON g.GuestId = b.GuestId
-LEFT JOIN 
-    Invoices i ON b.BookingId = i.BookingId
-ORDER BY 
-    g.GuestId;
 
-	--
+--4: (VG) Skapa MS-SQL syntaxer som kan utföra ’Joins’, ’Group By’ och minst en ’Subquery’ mot din databas
 
-	SELECT 
-    r.RoomId,
-    r.Type AS RoomType,
-    r.PricePerNight,
-    r.SizeInSquareMeters,
-    r.ExtraBeds,
-    r.TotalPeople AS MaxOccupancy,
-    CASE 
-        WHEN r.IsAvailable = 1 THEN 'Available'
-        ELSE 'Occupied'
-    END AS AvailabilityStatus,
-    b.BookingId,
-    b.GuestId,
-    b.CheckInDate,
-    b.CheckOutDate,
-    b.IsCheckedIn,
-    b.IsCheckedOut,
-    g.FirstName AS GuestFirstName,
-    g.LastName AS GuestLastName,
-    i.TotalAmount AS InvoiceAmount,
-    CASE 
-        WHEN i.IsPaid = 1 THEN 'Paid'
-        WHEN i.IsPaid = 0 THEN 'Unpaid'
-        ELSE 'No Invoice'
-    END AS PaymentStatus
-FROM 
-    Rooms r
-LEFT JOIN 
-    Bookings b ON r.RoomId = b.RoomId AND b.IsCheckedOut = 0 -- Endast aktiva bokningar
-LEFT JOIN 
-    Guests g ON b.GuestId = g.GuestId
-LEFT JOIN 
-    Invoices i ON b.BookingId = i.BookingId
-ORDER BY 
-    r.RoomId;
+-- Joins
 
-
--- ========================
--- 3. ALLMÄN GÄST- OCH BOKNINGSINFORMATION
--- ========================
-SELECT 
-    g.GuestId,
-    g.FirstName,
-    g.LastName,
-    g.Email,
-    g.PhoneNumber,
-    b.BookingId,
-    b.RoomId,
-    b.CheckInDate,
-    b.CheckOutDate,
-    b.IsCheckedIn,
-    b.IsCheckedOut,
-    b.BookingStatus
-FROM 
-    Guests g
-LEFT JOIN 
-    Bookings b ON g.GuestId = b.GuestId
-ORDER BY 
-    g.GuestId;
-
--- ========================
--- 4. STANDARD SELECT-STATEMENTS
--- ========================
-SELECT * FROM Guests;
-SELECT * FROM Invoices;
-SELECT * FROM Payments;
-SELECT * FROM Bookings;
-SELECT * FROM Rooms;
-
--- ========================
--- 5. ADMINISTRATIVA UPPGIFTER
--- ========================
-UPDATE Rooms SET IsAvailable = 1; -- Gör alla rum tillgängliga
-DELETE FROM Bookings; -- Tar bort alla aktuella bokningar
-DBCC CHECKIDENT ('Bookings', RESEED, 0); -- Återställ ID-sekvens för tabellen 'Bookings'
-
--- ========================
--- 6. GÄST- OCH RUMSINFORMATION MED CHECKINS
--- ========================
-SELECT 
-    B.BookingId, 
-    G.FirstName + ' ' + G.LastName AS GuestName, 
-    R.RoomId, 
-    R.Type AS RoomType, 
-    B.CheckInDate, 
-    B.CheckOutDate
-FROM 
-    Bookings AS B
-INNER JOIN 
-    Guests AS G ON B.GuestId = G.GuestId
-INNER JOIN 
-    Rooms AS R ON B.RoomId = R.RoomId
-WHERE 
-    G.LastName = 'Lastname2' 
-    AND B.IsCheckedOut = 0;
-
--- ========================
--- 7. SORTERA TILLGÄNGLIGA RUM EFTER PRIS
--- ========================
 SELECT 
     RoomId, 
     Type AS RoomType, 
@@ -193,121 +61,53 @@ WHERE
 ORDER BY 
     PricePerNight ASC;
 
--- ========================
--- 8. BOKNINGAR INOM EN SPECIFIK PERIOD
--- ========================
+	
 SELECT 
-    B.BookingId, 
-    G.FirstName + ' ' + G.LastName AS GuestName, 
-    R.RoomId, 
-    B.CheckInDate, 
-    B.CheckOutDate
+    g.GuestId,
+    g.FirstName,
+    g.LastName,
+    g.Email,
+    g.PhoneNumber,
+    b.BookingId,
+    b.RoomId,
+    b.CheckInDate,
+    b.CheckOutDate,
+    b.IsCheckedIn,
+    b.IsCheckedOut,
+    b.BookingCompleted
 FROM 
-    Bookings AS B
-INNER JOIN 
-    Guests AS G ON B.GuestId = G.GuestId
-INNER JOIN 
-    Rooms AS R ON B.RoomId = R.RoomId
-WHERE 
-    B.CheckInDate BETWEEN '2024-01-01' AND '2024-01-31'
+    Guests g
+LEFT JOIN 
+    Bookings b ON g.GuestId = b.GuestId
 ORDER BY 
-    B.CheckInDate ASC;
+    g.GuestId;
 
--- ========================
--- 9. GÄSTER MED EFTERNAMN SOM BÖRJAR MED 'J'
--- ========================
-SELECT 
-    GuestId, 
-    FirstName, 
-    LastName, 
-    Email, 
-    PhoneNumber
-FROM 
-    Guests
-WHERE 
-    LastName LIKE 'J%'
-ORDER BY 
-    LastName ASC;
 
--- ========================
--- 10. BOKNINGAR MED NULL CHECKOUT-DATUM
--- ========================
-SELECT 
-    BookingId, 
-    RoomId, 
-    GuestId, 
-    CheckInDate, 
-    CheckOutDate
-FROM 
-    Bookings
-WHERE 
-    CheckOutDate IS NULL
-ORDER BY 
-    CheckInDate DESC;
+--Group By
 
--- ========================
--- 11. JOINS: AKTIVA BOKNINGAR MED RUMSINFORMATION
--- ========================
-SELECT 
-    G.GuestId, 
-    G.FirstName + ' ' + G.LastName AS GuestName,
-    B.BookingId,
-    R.RoomId,
-    R.Type AS RoomType,
-    R.PricePerNight,
-    B.CheckInDate,
-    B.CheckOutDate
-FROM 
-    Guests AS G
-INNER JOIN 
-    Bookings AS B ON G.GuestId = B.GuestId
-INNER JOIN 
-    Rooms AS R ON B.RoomId = R.RoomId
-WHERE 
-    B.IsCheckedOut = 0 -- Endast aktiva bokningar
-ORDER BY 
-    G.GuestId;
+-- räknar antal bokningar per gäst har
+SELECT g.GuestId, COUNT(b.BookingId) AS BookingCount
+FROM Guests g
+LEFT JOIN Bookings b ON g.GuestId = b.GuestId
+GROUP BY g.GuestId;
 
--- ========================
--- 12. GROUP BY: BOKNINGAR OCH TOTAL INTÄKT PER RUMSTYP
--- ========================
-SELECT 
-    R.Type AS RoomType,
-    COUNT(B.BookingId) AS TotalBookings,
-    SUM(R.PricePerNight) AS TotalRevenue
-FROM 
-    Bookings AS B
-INNER JOIN 
-    Rooms AS R ON B.RoomId = R.RoomId
-GROUP BY 
-    R.Type
-ORDER BY 
-    TotalRevenue DESC;
 
--- ========================
--- 13. OBEGLIGNA BOKNINGAR ÄLDRE ÄN 10 DAGAR
--- ========================
-SELECT 
-    B.BookingId,
-    G.FirstName + ' ' + G.LastName AS GuestName,
-    R.RoomId,
-    I.TotalAmount,
-    I.PaymentDeadline
-FROM 
-    Bookings AS B
-INNER JOIN 
-    Guests AS G ON B.GuestId = G.GuestId
-INNER JOIN 
-    Rooms AS R ON B.RoomId = R.RoomId
-INNER JOIN 
-    Invoices AS I ON B.BookingId = I.BookingId
-WHERE 
-    I.IsPaid = 0 
-    AND I.PaymentDeadline < DATEADD(DAY, -10, GETDATE()) 
-    AND B.BookingId IN (
-        SELECT BookingId 
-        FROM Invoices 
-        WHERE IsPaid = 0
-    )
-ORDER BY 
-    I.PaymentDeadline ASC;
+
+-- SubQuery
+-- hämtar guest information i guest tabell, men samtidigt från bookings
+SELECT *
+FROM Guests g
+WHERE g.GuestId IN (
+    SELECT b.GuestId
+    FROM Bookings b
+);
+
+-- hämtar obetalda fakturar, bara om det inte finns hos payments tabellen
+SELECT i.InvoiceId, i.TotalAmount, i.IsPaid, i.PaymentDeadline
+FROM Invoices i
+WHERE i.InvoiceId NOT IN (
+    SELECT DISTINCT p.InvoiceId
+    FROM Payments p
+);
+
+
