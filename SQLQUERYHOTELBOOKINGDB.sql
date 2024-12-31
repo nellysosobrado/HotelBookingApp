c3e95b185823be664f6644bff4b2631f03db5126
Nelly <Nelly.sosobrado@gmail.com>
2024-12-31 14:17:35 +0100

    Folder for the hand ins examination

--- SQLQUERYHOTELBOOKINGDB.sql
+++
@@ -1,9 +0,0 @@
-USE HotelBookingDB
-
-SELECT * FROM Bookings
-SELECT * FROM Rooms
-SELECT * FROM Guests
-
-UPDATE Rooms SET IsAvailable =1; -- All room available
-DELETE FROM Bookings; --Remove all current bookings
-DBCC CHECKIDENT ('Bookings', RESEED, 0); -- resetest the ID seeding 
