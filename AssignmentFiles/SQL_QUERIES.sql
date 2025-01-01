02f013389668d3cd8179b4cb4551c95851b8179a
Nelly <Nelly.sosobrado@gmail.com>
2025-01-01 11:51:13 +0100

    fixed comments

--- AssignmentFiles/SQL_QUERIES.sql
+++ AssignmentFiles/SQL_QUERIES.sql
@@ -1,11 +1,7 @@
 USE HotelBookingDB;
 GO
 
-
---2: (G) Skapa MS-SQL syntaxer som kan utföra ’Select’, ’Where’, ’Order By’ uppgifter mot din databas.
-
-
--- Select
+-- Select ========================================================
 SELECT * FROM Rooms
 SELECT * FROM Guests
 SELECT * FROm Bookings
@@ -13,21 +9,19 @@ SELECT * FROM Invoices;
 SELECT * FROM Payments;
 SELECT * FROM CanceledBookingsHistory
 
--- Where
+-- Where ========================================================
 
--- Visar alla betalda invoices
 SELECT * 
 FROM Invoices
 WHERE isPaid = 1
 
--- -- Visar alla obetalda invoices
 SELECT * 
 FROM Invoices
 WHERE isPaid = 1
 
 
---Order By
--- sorterar utifrån invoiceid's
+--Order By ========================================================
+
 SELECT 
     i.InvoiceId,
     i.TotalAmount,
@@ -43,10 +37,7 @@ LEFT JOIN
 ORDER BY 
     i.InvoiceId;
 
-
---4: (VG) Skapa MS-SQL syntaxer som kan utföra ’Joins’, ’Group By’ och minst en ’Subquery’ mot din databas
-
--- Joins
+-- Joins ========================================================
 
 SELECT 
     RoomId, 
@@ -83,18 +74,16 @@ ORDER BY
     g.GuestId;
 
 
---Group By
+--Group By========================================================
 
--- räknar antal bokningar per gäst har
 SELECT g.GuestId, COUNT(b.BookingId) AS BookingCount
 FROM Guests g
 LEFT JOIN Bookings b ON g.GuestId = b.GuestId
 GROUP BY g.GuestId;
 
 
+-- SubQuery ========================================================
 
--- SubQuery
--- hämtar guest information i guest tabell, men samtidigt från bookings
 SELECT *
 FROM Guests g
 WHERE g.GuestId IN (
@@ -102,7 +91,7 @@ WHERE g.GuestId IN (
     FROM Bookings b
 );
 
--- hämtar obetalda fakturar, bara om det inte finns hos payments tabellen
+
 SELECT i.InvoiceId, i.TotalAmount, i.IsPaid, i.PaymentDeadline
 FROM Invoices i
 WHERE i.InvoiceId NOT IN (
